cc1f2125a53fee7b699e6e5b085c9330b3ffe7fe
ysugimoto <sugimoto@wnotes.net>
2018-08-03 10:09:31 +0900

    Add codec

---
+++ message/codec.go
@@ -0,0 +1,37 @@
+package message
+
+func encodeBool(b bool) (i int) {
+	if b {
+		i = 1
+	}
+	return
+}
+
+func decodeBool(i int) (b bool) {
+	if i > 0 {
+		b = true
+	}
+	return
+}
+
+func encodeString(str string) []byte {
+	buf := []byte(str)
+	size := len(buf)
+
+	return append(encodeInt16(size), buf...)
+}
+
+func decodeString(buffer []byte, start int) (string, int) {
+	size := ((int(buffer[start]) << 8) | int(buffer[start+1]))
+	start += 2
+	data := string(buffer[start:(start + size)])
+	return data, start + size
+}
+
+func encodeInt(v int) []byte {
+	return append([]byte{}, byte(v>>8), byte(v&0xFF))
+}
+
+func decodeInt(buffer []byte, start int) (int, int) {
+	return ((int(buffer[start]) << 8) | int(buffer[start+1])), start + 2
+}
